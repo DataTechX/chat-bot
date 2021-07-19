08d56d7f78c389faa96e6b4f341665f31ab88c6d
Lynn_ <69198341+JKTheRipperTH@users.noreply.github.com>
2021-07-19 16:25:15 +0700

    Add files via upload

---
+++ bot-chat.js
@@ -0,0 +1,33 @@
+/**
+ * @INFO
+ * ( Documentation NpmJS)
+ * - Discord-Chatbot: https://npmjs.com/package/discord-cha...
+ * - Discord-Reply: https://npmjs.com/package/discord-reply
+ * - Discord.js: https://npmjs.com/package/discord.js
+ */
+
+
+const Discord = require('discord.js')
+require('discord-reply');
+const client = new Discord.Client();
+const bot = require('discord-chatbot');
+const chatbot = new bot({ name: "NAME BOT"});
+const channelID = "CHANNEL ID";
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ready`);
+    client.channels.cache.get(channelID).send('Halo 🤑')
+});
+client.on('message', async (message) => {
+    if (
+        message.author.bot &&
+        message.channel.id === channelID &&
+        message.author.id === client.user.id
+    ) {
+        setTimeout(async () => {
+            const text = await chatbot.chat(message.content);
+            message.lineReply(text);
+        }, 5000);
+    }
+});
+
+client.login('TOKEN HERE')
